5942f394d9985a154877c0da7274e6ca0ed561a0
adamokennie <adamokennie@gmail.com>
2024-12-19 14:31:03 +0100

    middleware

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import registerUser from '../controller/registerUser.js';
 import { loginUser, logOut }  from '../controller/login.js';
 import { updatePassword, forgotPassword, resetPassword } from '../controller/passwordController.js';
-import Secure from '../middleware/AuthMiddleware.js';
+import Secure from '../middleware/authMiddleware.js';
 // import confirmEmail from '../controllers/confirmEmail.js';
 
 
@@ -11,6 +11,7 @@ const router = express.Router();
 
 //Routes
 router.post('/register', registerUser);
+
 router.post("/login",loginUser)
 router.get("/logout",logOut)
 router.patch('/updatepassword',Secure, updatePassword);
