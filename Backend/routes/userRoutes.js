7846dc86ef6b3c266f62282c7546ed7884eb537e
adamokennie <adamokennie@gmail.com>
2024-12-19 13:48:29 +0100

    update-password

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import registerUser from '../controller/registerUser.js';
 import { loginUser, logOut }  from '../controller/login.js';
 import { updatePassword, forgotPassword, resetPassword } from '../controller/passwordController.js';
-import Secure from '../middleware/AuthMiddleware.js';
+// import Secure from '../middleware/AuthMiddleware.js';
 // import confirmEmail from '../controllers/confirmEmail.js';
 
 
@@ -11,10 +11,9 @@ const router = express.Router();
 
 //Routes
 router.post('/register', registerUser);
-
 router.post("/login",loginUser)
 router.get("/logout",logOut)
-router.patch('/updatepassword',Secure, updatePassword);
+router.patch('/updatepassword', updatePassword);
 router.post('/forgotpassword',  forgotPassword);
 router.put('/resetpassword/:resetToken',  resetPassword);
 export default router; 
