5f8d3d0156cc943089a6e72c2204f7859e24bb4e
thianah <ekundayo6789@gmail.com>
2024-12-20 11:11:23 +0100

    delete account

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import registerUser from '../controller/registerUser.js';
 import { loginUser, logOut }  from '../controller/login.js';
 import { updatePassword, forgotPassword, resetPassword } from '../controller/passwordController.js';
 import Secure from '../middleware/authMiddleware.js';
+import { deleteAccount } from '../controller/deleteAccount.js';
 // import confirmEmail from '../controllers/confirmEmail.js';
 
 
@@ -17,4 +18,5 @@ router.get("/logout",logOut)
 router.patch('/updatepassword',Secure, updatePassword);
 router.post('/forgotpassword',  forgotPassword);
 router.put('/resetpassword/:resetToken',  resetPassword);
+router.delete("/delete-account/:email?/:userId?",deleteAccount);
 export default router; 
