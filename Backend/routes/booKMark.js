48dbf59ecafb013e4a1c5d4752b2a6b005bd10b3
lambojac <adamokennie@gmail.com>
2025-01-10 11:04:58 +0100

    middleware removed

--- Backend/routes/booKMark.js
+++ Backend/routes/booKMark.js
@@ -1,12 +1,11 @@
 import  express from 'express';
 import  bookMarkController from '../controller/eventBookmark.js';
 import upload from "../middleware/multer.js"
-import Secure from '../middleware/authMiddleware.js';
 const  router = express.Router();
-router.get('/:id/profile', Secure,bookMarkController.getProfile);
-router.get('/:id/bookmarks',Secure, bookMarkController.getBookmarkedEvents);
-router.post('/:userId/bookmarks/:eventId',Secure, bookMarkController.bookmarkEvent);
-router.delete('/:userId/bookmarks/:eventId',Secure, bookMarkController.removeBookmarkedEvent);
+router.get('/:id/profile', bookMarkController.getProfile);
+router.get('/:id/bookmarks', bookMarkController.getBookmarkedEvents);
+router.post('/:userId/bookmarks/:eventId', bookMarkController.bookmarkEvent);
+router.delete('/:userId/bookmarks/:eventId', bookMarkController.removeBookmarkedEvent);
 
 
 export default router; 
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,10 +2,7 @@ import express from 'express';
 import registerUser from '../controller/registerUser.js';
 import { loginUser, logOut }  from '../controller/login.js';
 import { updatePassword, forgotPassword, resetPassword } from '../controller/passwordController.js';
-import  bookMarkController from '../controller/eventBookmark.js';
-import Secure from '../middleware/authMiddleware.js';
 import { deleteAccount } from '../controller/deleteAccount.js';
-import upload from "../middleware/multer.js"
 // import confirmEmail from '../controllers/confirmEmail.js';
 
 
@@ -17,9 +14,8 @@ router.post('/register', registerUser);
 
 router.post("/login",loginUser)
 router.get("/logout",logOut)
-router.patch('/updatepassword',Secure, updatePassword);
+router.patch('/updatepassword', updatePassword);
 router.post('/forgotpassword',  forgotPassword);
 router.put('/resetpassword/:resetToken',  resetPassword);
 router.delete("/delete-account/:email?/:userId?",deleteAccount);
-router.put('/profile/:id',upload.single('image'), bookMarkController.updateProfile);
 export default router; 
